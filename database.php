039e95f1481e4f08247f136ec37602e865b5e370
Husian Turki <hturki4444@gmail.com>
2025-04-29 02:41:43 +0300

    the search bar work and added the searchResoult page and it's styling

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db = new PDO('mysql:host=localhost;dbname=ink_panels', 'root', 'root');
+    $db = new PDO('mysql:host=localhost;dbname=ink_panels', 'root', '0509219409');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::PARAM_STR);
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch(PDOException $e) {
