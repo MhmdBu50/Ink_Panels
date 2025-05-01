641b0a71ca935e30371567315583e4a5b1eef191
Mohammed Bukhamsin <moohaammeedbu50@gmail.com>
2025-05-01 11:55:42 +0300

    added hover effects for buttons

--- Product_details.php
+++ Product_details.php
@@ -170,18 +170,18 @@
                         <span id="total-price-amount"><?php echo $row['price'] ?></span></p>
                 </div>
                 <div class="add-to-cart" style="grid-area: box3-6;">
-                        <form method="post" action="product_details.php?id=<?php echo $product_id; ?>" >
-                            <input type="hidden" name="add_to_cart" value="1">
-                            <input type="hidden" name="product_id" value="<?php echo $row['MC_ID'] ?>">
-                            <input type="hidden" id="selected-quantity" name="quantity" value="1">
-                            <button type="submit" id="add-to-cart">
-                                <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M2 2H10L15.36 28.78C15.5429 29.7008 16.0438 30.5279 16.7751 31.1166C17.5064 31.7053 18.4214 32.018 19.36 32H38.8C39.7386 32.018 40.6536 31.7053 41.3849 31.1166C42.1162 30.5279 42.6171 29.7008 42.8 28.78L46 12H12M20 42C20 43.1046 19.1046 44 18 44C16.8954 44 16 43.1046 16 42C16 40.8954 16.8954 40 18 40C19.1046 40 20 40.8954 20 42ZM42 42C42 43.1046 41.1046 44 40 44C38.8954 44 38 43.1046 38 42C38 40.8954 38.8954 40 40 40C41.1046 40 42 40.8954 42 42Z" stroke="#1E1E1E" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-                                </svg>
-                                Add to cart
-                            </button>
-                        </form>
-                        <button class="you-are-rich add-to-cart" class="add"><strong>Buy all Volumes</strong></button>
+                    <form method="post" action="product_details.php?id=<?php echo $product_id; ?>" >
+                        <input type="hidden" name="add_to_cart" value="1">
+                        <input type="hidden" name="product_id" value="<?php echo $row['MC_ID'] ?>">
+                        <input type="hidden" id="selected-quantity" name="quantity" value="1">
+                        <button type="submit" id="add-to-cart" class="button-base">
+                            <svg width="50" height="48" viewBox="0 0 60 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M2 2H10L15.36 28.78C15.5429 29.7008 16.0438 30.5279 16.7751 31.1166C17.5064 31.7053 18.4214 32.018 19.36 32H38.8C39.7386 32.018 40.6536 31.7053 41.3849 31.1166C42.1162 30.5279 42.6171 29.7008 42.8 28.78L46 12H12M20 42C20 43.1046 19.1046 44 18 44C16.8954 44 16 43.1046 16 42C16 40.8954 16.8954 40 18 40C19.1046 40 20 40.8954 20 42ZM42 42C42 43.1046 41.1046 44 40 44C38.8954 44 38 43.1046 38 42C38 40.8954 38.8954 40 40 40C41.1046 40 42 40.8954 42 42Z" stroke="#1E1E1E" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            Add to cart
+                        </button>
+                    </form>
+                    <button class="you-are-rich button-base add-to-cart" class="add"><span>Buy now</span></button>
 
 </div>
 
